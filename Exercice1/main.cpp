5893e26065130cbd8bb28c0cfb1e4e03417fa0e6
Augustin <a.reille@net.estia.fr>
2017-10-16 16:55:48 +0100

    adding randomized vector test

--- Exercice1/main.cpp
+++ Exercice1/main.cpp
@@ -4,22 +4,36 @@
 
 int main()
 {
-    int vsize = 0;
-    std::cout << "Size of the vector : " << std::endl;
-    std::cin >> vsize;
+    // // With User vector input
+    // int vsize = 0;
+    // std::cout << "Size of the vector : " << std::endl;
+    // std::cin >> vsize;
 
-    std::vector<int> v(vsize, 0);
+    // std::vector<int> v(vsize, 0);
 
-    for (int i = 0; i < vsize; i++)
+    // for (int i = 0; i < vsize; i++)
+    // {
+    //     std::cout << "Element " << i << " of the vector :" << std::endl;
+    //     std::cin >> v[i];
+    // }
+
+    // With algorithmic vector filling
+    std::vector<int> v;
+
+    srand((unsigned)time(NULL));
+    // int a = rand() % 20 + 1; // random size between 0 & 20
+    int a = 10000; // 1000-sized vector
+    for (int i = 0; i < a; i++)
     {
-        std::cout << "Element " << i << " of the vector :" << std::endl;
-        std::cin >> v[i];
+        int b = rand() % 50 + 1;
+        v.push_back(b);
+        std::cout << v[i] << " ";
     }
 
-    BubbleSort(v);
-    // MergeSort(v);
+    // BubbleSort(v);
+    MergeSort(v);
 
-    std::cout << "Your sorted vector is : "<< std::endl;
+    std::cout << "Your sorted vector is : " << std::endl;
     for (int i = 0; i < v.size(); i++)
     {
         std::cout << v[i] << " ";
