f0a90c94d168f1130966593c00dd0a747a45f73a
Augustin <a.reille@net.estia.fr>
2017-10-17 15:28:30 +0100

    documentation

--- Exercice1/main.cpp
+++ Exercice1/main.cpp
@@ -2,37 +2,56 @@
 #include <vector>
 #include "IntegerArraySort.h"
 
+/*
+* Used to create a vector.
+* If the boolean is true, the vector will be randomly created
+* Else, the user will be asked to fill manually the vector
+*/
+std::vector<int> useAutoVectorFilling(bool b)
+{
+    if (b)
+    {
+        // Case true : random creation of the vector
+        std::vector<int> v;
+        srand((unsigned)time(NULL));
+        int a = rand() % 10000 + 1; // creating a random size between 0 & 10.000
+        for (int i = 0; i < a; i++)
+        {
+            int b = rand() % 1000 + 1; // creating a random value between 0 & 1.000
+            v.push_back(b);            // put the previous value in the vector
+        }
+        return v;
+    }
+    else
+    {
+        // Case false : user creates the vector
+        int vsize = 0;
+        std::cout << "Size of the vector : " << std::endl;
+        std::cin >> vsize;
+
+        std::vector<int> v(vsize, 0);
+        for (int i = 0; i < vsize; i++)
+        {
+            std::cout << "Element " << i << " of the vector :" << std::endl;
+            std::cin >> v[i];
+        }
+
+        return v;
+    }
+}
+
 int main()
 {
-    // // With User vector input
-    // int vsize = 0;
-    // std::cout << "Size of the vector : " << std::endl;
-    // std::cin >> vsize;
 
-    // std::vector<int> v(vsize, 0);
-
-    // for (int i = 0; i < vsize; i++)
-    // {
-    //     std::cout << "Element " << i << " of the vector :" << std::endl;
-    //     std::cin >> v[i];
-    // }
-
-    // With algorithmic vector filling
-    std::vector<int> v;
-
-    srand((unsigned)time(NULL));
-    // int a = rand() % 20 + 1; // random size between 0 & 20
-    int a = 10000; // 1000-sized vector
-    for (int i = 0; i < a; i++)
-    {
-        int b = rand() % 1000 + 1;
-        v.push_back(b);
-        std::cout << v[i] << " ";
-    }
+    std::vector<int> v = useAutoVectorFilling(false);
     std::cout << std::endl;
     std::cout << std::endl;
     std::cout << std::endl;
 
+    /*
+    * Choose which method to use
+    */
+    
     // BubbleSort(v);
     MergeSort(v);
 
