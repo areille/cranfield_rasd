8262360666d684abedc4535b44bedbb5b486c4ce
Augustin <a.reille@net.estia.fr>
2017-10-18 15:50:49 +0100

    error void

--- Exercice1/IntegerArraySort.h
+++ Exercice1/IntegerArraySort.h
@@ -4,7 +4,7 @@
 * These functions sort a vector of integers, and return the vector of indexes of the first vector
 */
 std::vector<int> BubbleSort(std::vector<int> &v);
-MergeSort(std::vector<int> &v);
+void MergeSort(std::vector<int> &v);
 
 // 3 functions for MergeSort
 void SplitAndMerge(std::vector<int> &w, int start, int end, std::vector<int> &v);
